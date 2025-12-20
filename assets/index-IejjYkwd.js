b436afabcb7f7197ff254fbc1959cbc02c7f335b
janbets <janbeeangeles@gmail.com>
2025-12-20 15:08:11 -0700

    Updates

--- assets/index-IejjYkwd.js
+++ assets/index-IejjYkwd.js
@@ -26904,7 +26904,7 @@ function Main() {
     ] })
   ] });
 }
-const version = "1.0.35";
+const version = "1.0.36";
 function App() {
   const [appReady, setAppReady] = reactExports.useState(false);
   reactExports.useEffect(() => {
