15d49b978d8d7c809a7bad55c523bf2233774c99
janbets <janbeeangeles@gmail.com>
2025-12-19 19:56:27 -0700

    Updates

--- assets/index-BkDPCT71.js
+++ assets/index-BkDPCT71.js
@@ -26880,7 +26880,7 @@ function Main() {
     ] })
   ] });
 }
-const version = "1.0.32";
+const version = "1.0.33";
 function App() {
   const [appReady, setAppReady] = reactExports.useState(false);
   reactExports.useEffect(() => {
