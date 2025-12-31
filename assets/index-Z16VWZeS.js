e062998bc131fb9895ad77d3ea0db7925764d6d7
janbets <janbeeangeles@gmail.com>
2025-12-30 18:24:11 -0700

    Updates

--- assets/index-Z16VWZeS.js
+++ assets/index-Z16VWZeS.js
@@ -26921,7 +26921,7 @@ function Main() {
     ] })
   ] });
 }
-const version = "1.0.52";
+const version = "1.0.53";
 function App() {
   const [appReady, setAppReady] = reactExports.useState(false);
   reactExports.useEffect(() => {
