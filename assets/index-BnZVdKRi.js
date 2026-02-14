0998ba2c523c8ed8da18223f7b916daf94f5e7fb
janbets <janbeeangeles@gmail.com>
2026-02-13 19:17:53 -0700

    Updates

--- assets/index-BnZVdKRi.js
+++ assets/index-BnZVdKRi.js
@@ -3,7 +3,7 @@ var __commonJS = (cb, mod) => function __require() {
   return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
 };
 var require_index_001 = __commonJS({
-  "assets/index-izIYOLhb.js"(exports, module) {
+  "assets/index-BnZVdKRi.js"(exports, module) {
     var commonjsGlobal = typeof globalThis !== "undefined" ? globalThis : typeof window !== "undefined" ? window : typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : {};
     function getDefaultExportFromCjs(x) {
       return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, "default") ? x["default"] : x;
@@ -21645,6 +21645,16 @@ ${suffix}`;
       const [loading, setLoading] = reactExports.useState(false);
       const [error, setError] = reactExports.useState(null);
       const [selectedUserBuilds, setSelectedUserBuilds] = reactExports.useState(/* @__PURE__ */ new Set());
+      reactExports.useEffect(() => {
+        if (window.api) {
+          window.api.onDeepLinkUser((account) => {
+            handleUserClick({ data: { build: account } });
+          });
+        }
+        return () => {
+          window.api?.removeDeepLinkListener?.();
+        };
+      }, []);
       const loadSelectedUsers = reactExports.useCallback(() => {
         const existingData = localStorage.getItem("selectedUsers");
         if (existingData) {
@@ -30371,7 +30381,7 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
         ] })
       ] });
     }
-    const version = "1.0.76";
+    const version = "1.0.77";
     function App() {
       const [appReady, setAppReady] = reactExports.useState(false);
       reactExports.useEffect(() => {
