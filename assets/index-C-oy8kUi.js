4d5c5f04954ac419c2d55b4089b605a275951164
janbets <janbeeangeles@gmail.com>
2026-02-13 20:16:20 -0700

    Updates

--- assets/index-C-oy8kUi.js
+++ assets/index-C-oy8kUi.js
@@ -3,7 +3,7 @@ var __commonJS = (cb, mod) => function __require() {
   return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
 };
 var require_index_001 = __commonJS({
-  "assets/index-eKnDnqlR.js"(exports, module) {
+  "assets/index-C-oy8kUi.js"(exports, module) {
     var commonjsGlobal = typeof globalThis !== "undefined" ? globalThis : typeof window !== "undefined" ? window : typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : {};
     function getDefaultExportFromCjs(x) {
       return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, "default") ? x["default"] : x;
@@ -27680,6 +27680,7 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
     var PNChannel = /* @__PURE__ */ ((PNChannel2) => {
       PNChannel2["LiveGameDetails"] = "LiveGameDetails";
       PNChannel2["OpenAccount"] = "OpenAccount";
+      PNChannel2["AccountCount"] = "AccountCount";
       return PNChannel2;
     })(PNChannel || {});
     const $PN = new PubNub({
@@ -27697,12 +27698,12 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
           }
         };
         $PN.subscribe({
-          channels: [PNChannel.OpenAccount + channel + "sub"]
+          channels: [PNChannel.OpenAccount + channel]
         });
         $PN.addListener(PNListener);
         return () => {
           $PN.unsubscribe({
-            channels: [PNChannel.OpenAccount + channel + "sub"]
+            channels: [PNChannel.OpenAccount + channel]
           });
           $PN.removeListener(PNListener);
         };
@@ -27734,9 +27735,10 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
       );
       const handlePublish = reactExports.useCallback((usersArray) => {
         $PN.publish({
-          channel: PNChannel.OpenAccount + window.api.getHostname() + "pub",
+          channel: PNChannel.AccountCount,
           message: {
-            accounts: usersArray.length
+            accounts: usersArray.length,
+            from: window.api.getHostname()
           }
         }).catch().finally();
       }, []);
@@ -30414,7 +30416,7 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
         ] })
       ] });
     }
-    const version = "1.0.79";
+    const version = "1.0.80";
     function App() {
       const [appReady, setAppReady] = reactExports.useState(false);
       reactExports.useEffect(() => {
