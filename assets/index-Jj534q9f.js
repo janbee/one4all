dad4b759e6b03f7070c7e9bc9f266363ff52f2e1
janbets <janbeeangeles@gmail.com>
2026-02-13 20:32:09 -0700

    Updates

--- assets/index-Jj534q9f.js
+++ assets/index-Jj534q9f.js
@@ -3,7 +3,7 @@ var __commonJS = (cb, mod) => function __require() {
   return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
 };
 var require_index_001 = __commonJS({
-  "assets/index-C-oy8kUi.js"(exports, module) {
+  "assets/index-Jj534q9f.js"(exports, module) {
     var commonjsGlobal = typeof globalThis !== "undefined" ? globalThis : typeof window !== "undefined" ? window : typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : {};
     function getDefaultExportFromCjs(x) {
       return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, "default") ? x["default"] : x;
@@ -27691,7 +27691,6 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
     const accounts = data;
     function usePubnub(channel, onMessage) {
       reactExports.useEffect(() => {
-        console.log("gaga-----------12312312--------------------------", channel);
         const PNListener = {
           message: function(event) {
             onMessage(event.message);
@@ -30416,7 +30415,7 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
         ] })
       ] });
     }
-    const version = "1.0.80";
+    const version = "1.0.81";
     function App() {
       const [appReady, setAppReady] = reactExports.useState(false);
       reactExports.useEffect(() => {
