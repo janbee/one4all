1790bc307f6d5aa2b5877cd70c35f41137cae4af
janbee <janbeeangeles@gmail.com>
2026-02-22 11:57:35 -0700

    Updates

--- assets/index-Wa-sNuJ9.js
+++ assets/index-Wa-sNuJ9.js
@@ -3,7 +3,7 @@ var __commonJS = (cb, mod) => function __require() {
   return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
 };
 var require_index_001 = __commonJS({
-  "assets/index-CgHg44d8.js"(exports, module) {
+  "assets/index-Wa-sNuJ9.js"(exports, module) {
     var commonjsGlobal = typeof globalThis !== "undefined" ? globalThis : typeof window !== "undefined" ? window : typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : {};
     function getDefaultExportFromCjs(x) {
       return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, "default") ? x["default"] : x;
@@ -28156,10 +28156,12 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
           {
             className: "h-full w-full",
             ref: webviewRef,
+            nodeintegration: true,
+            webpreferences: "contextIsolation=false, spellcheck=false",
             id: `PlayAbWebView-${account}`,
             partition: `persist:${account.toLowerCase()}`,
             src: `https://playalberta.ca/sports/live?t=${(/* @__PURE__ */ new Date()).getTime()}&account=${account}`,
-            preload: `file://${window.__preload}/play-ab.js`
+            preload: `file://${window.__preload.replace(/\/$/, "")}/play-ab.js`
           },
           `${account}-${reload}`
         ) })
@@ -30416,7 +30418,7 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
         ] })
       ] });
     }
-    const version = "1.0.111";
+    const version = "1.0.112";
     function App() {
       const [appReady, setAppReady] = reactExports.useState(false);
       reactExports.useEffect(() => {
