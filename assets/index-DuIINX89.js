3d9e8e316c97aff7beb4fd74b2bcb1747231a924
janbets <janbeeangeles@gmail.com>
2026-04-10 13:05:11 -0600

    Updates

--- assets/index-DuIINX89.js
+++ assets/index-DuIINX89.js
@@ -3,7 +3,7 @@ var __commonJS = (cb, mod) => function __require() {
   return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
 };
 var require_index_001 = __commonJS({
-  "assets/index-mLtMQXmh.js"(exports, module) {
+  "assets/index-DuIINX89.js"(exports, module) {
     var commonjsGlobal = typeof globalThis !== "undefined" ? globalThis : typeof window !== "undefined" ? window : typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : {};
     function getDefaultExportFromCjs(x) {
       return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, "default") ? x["default"] : x;
@@ -30291,7 +30291,7 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
             label: toMoney(totalStaked)
           }
         ),
-        weeklySummary?.data.done + ""
+        weeklySummary?.data.done + " - " + weekStart.toISOString()
       ] });
     }
     function Bets({ user }) {
@@ -30445,7 +30445,7 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
         ] })
       ] });
     }
-    const version = "1.0.139";
+    const version = "1.0.140";
     function App() {
       const [appReady, setAppReady] = reactExports.useState(false);
       reactExports.useEffect(() => {
