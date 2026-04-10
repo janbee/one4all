6def7d0b2f2c1b40df0ac78c3c0ea2ad3605c192
janbets <janbeeangeles@gmail.com>
2026-04-10 13:01:39 -0600

    Updates

--- assets/index-BIRA-9S5.js
+++ assets/index-BIRA-9S5.js
@@ -3,7 +3,7 @@ var __commonJS = (cb, mod) => function __require() {
   return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
 };
 var require_index_001 = __commonJS({
-  "assets/index-BUn67hLm.js"(exports, module) {
+  "assets/index-BIRA-9S5.js"(exports, module) {
     var commonjsGlobal = typeof globalThis !== "undefined" ? globalThis : typeof window !== "undefined" ? window : typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : {};
     function getDefaultExportFromCjs(x) {
       return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, "default") ? x["default"] : x;
@@ -30277,20 +30277,22 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
       if (!isDone) {
         const { weeklySummary: weeklySummary2 } = useMainStore(user.data.build);
         totalStaked = weeklySummary2?.data.totalStaked || 0;
-        isDone = weeklySummary2?.data.done === true && weeklySummary2?.data.openBets === 0;
       }
-      return /* @__PURE__ */ jsxRuntimeExports.jsx(TableCell, { className: "md:w-full", textAlign: "center", children: /* @__PURE__ */ jsxRuntimeExports.jsx(
-        Progress,
-        {
-          inverted: true,
-          success: isDone,
-          precision: 0,
-          value: Math.floor(totalStaked),
-          progress: "percent",
-          total: 500,
-          label: toMoney(totalStaked)
-        }
-      ) });
+      return /* @__PURE__ */ jsxRuntimeExports.jsxs(TableCell, { className: "md:w-full", textAlign: "center", children: [
+        /* @__PURE__ */ jsxRuntimeExports.jsx(
+          Progress,
+          {
+            inverted: true,
+            success: isDone,
+            precision: 0,
+            value: Math.floor(totalStaked),
+            progress: "percent",
+            total: 500,
+            label: toMoney(totalStaked)
+          }
+        ),
+        isDone + ""
+      ] });
     }
     function Bets({ user }) {
       const { weeklySummary } = useMainStore(user.data.build);
@@ -30443,7 +30445,7 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
         ] })
       ] });
     }
-    const version = "1.0.137";
+    const version = "1.0.138";
     function App() {
       const [appReady, setAppReady] = reactExports.useState(false);
       reactExports.useEffect(() => {
