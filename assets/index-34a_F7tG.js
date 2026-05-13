7087b3eedc653b274320ee986d7a9fc96c159d49
janbets <janbeeangeles@gmail.com>
2026-05-13 11:45:20 -0600

    Updates

--- assets/index-34a_F7tG.js
+++ assets/index-34a_F7tG.js
@@ -3,7 +3,7 @@ var __commonJS = (cb, mod) => function __require() {
   return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
 };
 var require_index_001 = __commonJS({
-  "assets/index-DXsojbwA.js"(exports, module) {
+  "assets/index-34a_F7tG.js"(exports, module) {
     var commonjsGlobal = typeof globalThis !== "undefined" ? globalThis : typeof window !== "undefined" ? window : typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : {};
     function getDefaultExportFromCjs(x) {
       return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, "default") ? x["default"] : x;
@@ -35787,17 +35787,17 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
       });
       const bonus = weeklySummary?.data.bonuses?.[0] || { Amount: 0 };
       const potentialBonus = lastWeeklySummary?.data.potentialBonus || 0;
+      const isRed = bonus?.Amount > 0 && bonus?.Amount < potentialBonus;
+      const isGreen = bonus?.Amount > 0 && bonus?.Amount === potentialBonus;
       return /* @__PURE__ */ jsxRuntimeExports.jsx(TableCell, { children: /* @__PURE__ */ jsxRuntimeExports.jsxs("div", { className: "flex justify-between", children: [
-        /* @__PURE__ */ jsxRuntimeExports.jsxs(
+        /* @__PURE__ */ jsxRuntimeExports.jsx(
           "span",
           {
             className: classNames({
-              "text-green-dark": bonus?.Amount > 0
+              "text-green-dark": isGreen,
+              "text-red-dark": isRed
             }),
-            children: [
-              " ",
-              toMoney(bonus?.Amount, 0)
-            ]
+            children: toMoney(bonus?.Amount, 0)
           }
         ),
         /* @__PURE__ */ jsxRuntimeExports.jsx("span", { children: "-" }),
@@ -35931,7 +35931,7 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
         ] })
       ] });
     }
-    const version = "1.0.156";
+    const version = "1.0.157";
     function App() {
       const [appReady, setAppReady] = reactExports.useState(false);
       reactExports.useEffect(() => {
