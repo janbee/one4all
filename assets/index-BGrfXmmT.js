009b193527366661bf67554b7ea85cde7f6da56f
janbets <janbeeangeles@gmail.com>
2026-06-03 20:01:37 -0600

    Updates

--- assets/index-BGrfXmmT.js
+++ assets/index-BGrfXmmT.js
@@ -3,7 +3,7 @@ var __commonJS = (cb, mod) => function __require() {
   return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
 };
 var require_index_001 = __commonJS({
-  "assets/index--T876gK_.js"(exports, module) {
+  "assets/index-BGrfXmmT.js"(exports, module) {
     var commonjsGlobal = typeof globalThis !== "undefined" ? globalThis : typeof window !== "undefined" ? window : typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : {};
     function getDefaultExportFromCjs(x) {
       return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, "default") ? x["default"] : x;
@@ -44846,9 +44846,9 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
       const [showLogsOnAccount, setShowLogs] = reactExports.useState("");
       const tableCols = [
         {
-          name: /* @__PURE__ */ jsxRuntimeExports.jsx(jsxRuntimeExports.Fragment, { children: "AppBuild" }),
+          name: /* @__PURE__ */ jsxRuntimeExports.jsx("span", { className: "cursor-pointer", onClick: handleStartAll, children: "AppBuild" }),
           render: (user) => {
-            return /* @__PURE__ */ jsxRuntimeExports.jsx("div", { onClick: handleStartAll, children: /* @__PURE__ */ jsxRuntimeExports.jsx(Build, { user }) });
+            return /* @__PURE__ */ jsxRuntimeExports.jsx(Build, { user });
           },
           className: "w-[75px]"
         },
@@ -44959,7 +44959,7 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
         ] })
       ] });
     }
-    const version = "1.0.187";
+    const version = "1.0.188";
     function App() {
       const [appReady, setAppReady] = reactExports.useState(false);
       reactExports.useEffect(() => {
