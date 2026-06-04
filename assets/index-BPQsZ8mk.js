cbf9a2921e82b129f7c3eb468d9271c073efeb38
janbets <janbeeangeles@gmail.com>
2026-06-03 18:09:31 -0600

    Updates

--- assets/index-BPQsZ8mk.js
+++ assets/index-BPQsZ8mk.js
@@ -3,7 +3,7 @@ var __commonJS = (cb, mod) => function __require() {
   return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
 };
 var require_index_001 = __commonJS({
-  "assets/index-BgaNJ-xM.js"(exports, module) {
+  "assets/index-BPQsZ8mk.js"(exports, module) {
     var commonjsGlobal = typeof globalThis !== "undefined" ? globalThis : typeof window !== "undefined" ? window : typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : {};
     function getDefaultExportFromCjs(x) {
       return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, "default") ? x["default"] : x;
@@ -42652,7 +42652,7 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
       }
     }
     function tryParseJson(value) {
-      const trimmed = value.trim();
+      const trimmed = value?.trim();
       if (!(trimmed.startsWith("{") || trimmed.startsWith("["))) return null;
       try {
         const parsed = JSON.parse(trimmed);
@@ -44939,7 +44939,7 @@ ${s2}` }))), `v2.${this.hasher(s2, this.secretKey)}`.replace(/\+/g, "-").replace
         ] })
       ] });
     }
-    const version = "1.0.180";
+    const version = "1.0.181";
     function App() {
       const [appReady, setAppReady] = reactExports.useState(false);
       reactExports.useEffect(() => {
